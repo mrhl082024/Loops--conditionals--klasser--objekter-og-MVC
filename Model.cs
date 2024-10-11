b194800e708f8ab4f349e4ed54a97efc92a063e4
mrhl082024 <mrhl082024@gmail.com>
2024-10-11 11:59:58 +0200

    lagt til model class

--- Model.cs
+++ Model.cs
@@ -4,5 +4,28 @@ public class VideoGame
     public int Year {get; set;}
     public string Genre {get; set;}
 
-    
+    public VideoGame(string title, int year, string genre)   
+    {
+        Title=title;
+        Year=year;
+        Genre=genre;
+    }
+
+    public override string ToString()
+    {
+        return $"Title: {Title}. Release year: {Year}. Genre: {Genre}";
+    }
+}
+
+public class Model
+{
+    public List<VideoGame> VideoGames {get; set;}
+    public Model ()
+    {
+        VideoGames = new List<VideoGame>
+        {
+        //Legg til spill her
+        new VideoGame("Dark Souls", 2011, "Soulsborne, Difficult, Dark Fantasy")
+        };
+    }
 }
