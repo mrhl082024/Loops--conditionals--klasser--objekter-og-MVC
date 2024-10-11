63efdae265e1c82cd9d5e3801e90b986f1b80e20
mrhl082024 <mrhl082024@gmail.com>
2024-10-11 12:09:50 +0200

    lagt til updategametitle

--- Model.cs
+++ Model.cs
@@ -28,4 +28,41 @@ public class Model
         new VideoGame("Dark Souls", 2011, "Soulsborne, Difficult, Dark Fantasy")
         };
     }
+
+    public void AddGame(string title, int year, string genre)
+    {
+        VideoGames.Add(new VideoGame(title, year, genre));
+    }
+
+    public bool RemoveGame(string title)
+    {
+        var gameToRemove = VideoGames.Find(g => g.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        if(gameToRemove != null)
+        {
+            VideoGames.Remove(gameToRemove);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool UpdateGameTitle(string title, string newTitle)
+    {
+        var gameToUpdate = VideoGames.Find(g => g.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        if(gameToUpdate != null)
+        {
+            gameToUpdate.Title = newTitle;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+
 }
